e9cde59d22b2da85ad2d899e87c374c2d7eadaa8
Dexter Chan <dexterchann@gmail.com>
2020-04-04 19:31:12 +0000

    update image url

--- db/seeds/03_items.sql
+++ db/seeds/03_items.sql
@@ -1,5 +1,5 @@
 INSERT INTO items (name, description, price, photo_url)
-VALUES ('French Toast', 'with bananas and blueberries', 1100, 'https://unsplash.com/photos/zcUgjyqEwe8'),
+VALUES ('French Toast', 'with bananas and blueberries', 1100, 'https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60'),
 ('Grilled Porkchop', 'with broccolini and roast potatoes', 1800, 'https://images.unsplash.com/photo-1432139555190-58524dae6a55?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'),
 ('Pan-roasted Salmon', 'with potato puree and spinach', 2100, 'https://images.unsplash.com/photo-1467003909585-2f8a72700288?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'),
 ('Steak Tacos', 'with salsa verde', 800, 'https://images.unsplash.com/photo-1504544750208-dc0358e63f7f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'),
