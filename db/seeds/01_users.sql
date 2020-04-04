0103659502a707bfc2c78e7e7225b29b9d95be18
Dexter Chan <dexterchann@gmail.com>
2020-04-04 16:06:51 +0000

    add admin user seed

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,7 +1,7 @@
 -- Users table seeds here (Example)
 INSERT INTO users (first_name, last_name, email, password, dob, phone, post_code, type)
-VALUES ('Dexter', 'Chan', 'chandexter@gmail.com', 'password', '1994-09-30', '7781234568', 'V1Y2V2', 'admin');
-VALUES ('Jason', 'Park', 'parkjason@gmail.com', 'password', '1987-01-20', '7781234567', 'V1Y2V5', 'admin');
+VALUES ('Dexter', 'Chan', 'chandexter@gmail.com', 'password', '1994-09-30', '7781234568', 'V1Y2V2', 'admin'),
+('Jason', 'Park', 'parkjason@gmail.com', 'password', '1987-01-20', '7781234567', 'V1Y2V5', 'admin');
 
 INSERT INTO users (first_name, last_name, email, password, dob, phone, post_code)
 VALUES ('Jason', 'Park', 'jasonpark@gmail.com', 'password', '1987-01-20', '7781234567', 'V1Y2V5'),
