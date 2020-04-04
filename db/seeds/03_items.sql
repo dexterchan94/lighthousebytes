95ff34b65e70af95351552355bd3ee0e0325ff6a
Dexter Chan <dexterchann@gmail.com>
2020-04-04 04:05:02 +0000

    add seed data

---
+++ db/seeds/03_items.sql
@@ -0,0 +1,7 @@
+INSERT INTO items (name, description, price, photo_url)
+VALUES ('French Toast', 'with bananas and blueberries', 1100, 'https://unsplash.com/photos/zcUgjyqEwe8'),
+('Grilled Porkchop', 'with broccolini and roast potatoes', 1800, 'https://images.unsplash.com/photo-1432139555190-58524dae6a55?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'),
+('Pan-roasted Salmon', 'with potato puree and spinach', 2100, 'https://images.unsplash.com/photo-1467003909585-2f8a72700288?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'),
+('Steak Tacos', 'with salsa verde', 800, 'https://images.unsplash.com/photo-1504544750208-dc0358e63f7f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'),
+('Pan-seared Duck Breast', 'with seasonal roast vegetables', 2000, 'https://images.unsplash.com/photo-1580554530778-ca36943938b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'),
+('Green Salad', 'with italian vinaigrette', 900, 'https://images.unsplash.com/photo-1544982590-068dabc956dc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60');
---
+++ db/seeds/04_order_items.sql
@@ -0,0 +1,8 @@
+INSERT INTO order_items (order_id, item_id, quantity)
+VALUES (1, 1, 1),
+(1, 2, 1),
+(2, 4, 2),
+(2, 3, 1),
+(3, 5, 1),
+(3, 6, 1);
+
