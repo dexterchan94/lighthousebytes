db65713495f4044ec7566fae741da4537da57996
Junsung Park <zeipar@gmail.com>
2020-04-06 03:17:24 +0000

    wrote js to build and process items in the cart. Also made binding events to add/remove items to cart while disabling the checkout button if empty

--- public/scripts/newOrder.js
+++ public/scripts/newOrder.js
@@ -13,7 +13,35 @@ $(() => {
     }
   };
 
-  $('.counterBox button').click(function() {
+  const addItem = (itemId, name, qty, price) => {
+    const $cartItem = $('<div>').addClass('row');
+    $cartItem.attr('id', `cart-${itemId}`);
+
+    // item name
+    const $itemName = $('<div>').addClass('col-6');
+    $itemName.append(name);
+
+    // item quantity
+    const $itemQty = $('<div>').addClass('col-1');
+    $itemQty.append(qty);
+
+    // item price
+    const $itemPrice = $('<div>').addClass('col-3');
+    $itemPrice.append(`$${(price / 100).toFixed(2)}`);
+
+    // remove button
+    const $removeButton = $('<div>').addClass('col-1 rmv-btn');
+    const $removeIcon = $('<i>').addClass('far fa-trash-alt');
+    $removeButton.append($removeIcon);
+
+    // append name, quantity, price to $cartItem
+    $cartItem.append($itemName, $itemQty, $itemPrice, $removeButton);
+
+    console.log($cartItem);
+    return $cartItem;
+  };
+
+  $('.counterBox button').click(function () {
     let qty = this.parentNode.childNodes[3].value;
     // console.log(this.parentNode.childNodes[7].id);
 
@@ -29,18 +57,36 @@ $(() => {
     $(this).focus().select();
   });
 
+  // event bindig on dynamic elements
+  $('body').on('click', '.rmv-btn', function() {
+    $(this).parent().remove();
+
+    if ($('.row').length === 0) {
+      $('.cart-checkout').prop('disabled', true);
+    }
+  });
+
   $('.add-cart').click(function() {
-    const itemNo = this.parentNode.childNodes[1].childNodes[7].id;
-    const qty = this.parentNode.childNodes[1].childNodes[3].value
-    const price = this.parentNode.childNodes[1].childNodes[9].value;
+    const currentNodes = this.parentNode.childNodes[1];
+    const itemId = currentNodes.childNodes[7].id;
+    const qty = currentNodes.childNodes[3].value
+    const price = currentNodes.childNodes[9].value;
+    const name = this.parentNode.parentNode.childNodes[1].innerHTML;
 
     if (Number(qty) !== 0) {
-      console.log(itemNo, qty, price);
+      const $itemData = addItem(itemId, name, qty, price);
+      $('.cart-body-wrapper').append($itemData);
+
+      if ($('.row').length > 0) {
+        $('.cart-checkout').prop('disabled', false);
+      }
 
     } else {
       console.log('Please specify a nubmer of items you want');
     }
+  });
 
-  })
+
+
 
 });
