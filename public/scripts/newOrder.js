781b85e998178ad336f75e3865b9640b6e423896
Junsung Park <zeipar@gmail.com>
2020-04-06 00:30:11 +0000

    'add to cart' button now successfully fetches the id, price and quantity of the menu

--- public/scripts/newOrder.js
+++ public/scripts/newOrder.js
@@ -13,7 +13,7 @@ $(() => {
     }
   };
 
-  $('button').click(function() {
+  $('.counterBox button').click(function() {
     let qty = this.parentNode.childNodes[3].value;
     // console.log(this.parentNode.childNodes[7].id);
 
@@ -28,5 +28,19 @@ $(() => {
   $('.counter').click(function () {
     $(this).focus().select();
   });
+
+  $('.add-cart').click(function() {
+    const itemNo = this.parentNode.childNodes[1].childNodes[7].id;
+    const qty = this.parentNode.childNodes[1].childNodes[3].value
+    const price = this.parentNode.childNodes[1].childNodes[9].value;
+
+    if (Number(qty) !== 0) {
+      console.log(itemNo, qty, price);
+
+    } else {
+      console.log('Please specify a nubmer of items you want');
+    }
+
+  })
 
 });
