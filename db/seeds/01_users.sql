814a45bccb1e21a815488d0431337b0bc620876b
Junsung Park <zeipar@gmail.com>
2020-04-07 18:49:19 +0000

    user Jason now has valid phone number... :)

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -4,6 +4,6 @@ VALUES ('Dexter', 'Chan', 'chandexter@gmail.com', 'password', '1994-09-30', '778
 ('Jason', 'Park', 'parkjason@gmail.com', 'password', '1987-01-20', '7781234567', 'V1Y2V5', 'admin');
 
 INSERT INTO users (first_name, last_name, email, password, dob, phone, post_code)
-VALUES ('Jason', 'Park', 'jasonpark@gmail.com', 'password', '1987-01-20', '7781234567', 'V1Y2V5'),
+VALUES ('Jason', 'Park', 'jasonpark@gmail.com', 'password', '1987-01-20', '+17785122171', 'V1Y2V5'),
 ('Dexter', 'Chan', 'dexterchan@gmail.com', 'password', '1994-09-30', '+16046908791', 'V1Y2V2');
 
